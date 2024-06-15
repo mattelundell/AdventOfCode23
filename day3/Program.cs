b3790927937f94a265f470900bf776871db49c08
mattiaslundel1 <mattias.lundell@elva-group.com>
2024-06-15 19:09:48 +0200

    docs(day3): added initial todos

--- day3/Program.cs
+++ day3/Program.cs
@@ -4,3 +4,10 @@ internal class Program
 {
     static void Main(string[] args) { }
 }
+
+
+// TODO: parse input text to be able to iterate through each coordinate
+// TODO: find a number and identify how long it is, then check all adjacent tiles and see if any is a symbol. If yes, store the number. Might lead to unneccesary lookups?
+//       OR
+//       find a symbol and check the adjacent tiles to see if there is a digit, then identify the number. See if the number has been stored already, otherwise store it.
+// TODO: calculate the sum of the numbers that are adjacent to a symbol. 
