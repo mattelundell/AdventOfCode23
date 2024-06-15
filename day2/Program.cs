bab0ee68abef397f1991543130161cbddccfa02d
mattiaslundel1 <mattias.lundell@elva-group.com>
2024-06-15 08:57:52 +0200

    chore(day2): initialized new project for day 2 and added problem description

---
+++ day2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace day1;
+
+internal class Program
+{
+
+    static void Main(string[] args)
+    {
+        string inputPath = "input.txt";
+
+        List<string> inputWords = ReadWordsFromFile(inputPath);
+        //List<string> testWords1 = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
+        //List<string> testWords2 = ["oneight", "two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"];
+
+    }
+
+
+}
+
