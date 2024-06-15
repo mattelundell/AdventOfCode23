f3acea0a830918fd5fe95e017c8001a68908ee15
mattiaslundel1 <mattias.lundell@elva-group.com>
2024-06-15 12:26:51 +0200

    chore(day2): refactored testData to a txt file to make code cleaner

--- day2/Program.cs
+++ day2/Program.cs
@@ -18,18 +18,14 @@ internal class Program
 
     static void Main(string[] args)
     {
-        /* string testData =
-            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green" +
-            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue" +
-            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red" +
-            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red" +
-            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"; */
-        //Dictionary<int, List<int[]>> games = ParseInput(testData);
+        string inputPath = "test.txt";
+        string testData = File.ReadAllText(inputPath);
+        Dictionary<int, List<int[]>> games = ParseInput(testData);
 
-        string inputPath = "input.txt";
+        /* string inputPath = "input.txt";
         string inputData = File.ReadAllText(inputPath);
-        
-        Dictionary<int, List<int[]>> games = ParseInput(inputData);
+        Dictionary<int, List<int[]>> games = ParseInput(inputData); */
+
         PrintGames(games);
     }
 
