452b1a0c52bde423c307ff3ae9453ee148bb5ffe
mattiaslundel1 <mattias.lundell@elva-group.com>
2024-06-17 11:36:29 +0200

    feat(day3): implemented solution for part one of the problem

--- day3/Program.cs
+++ day3/Program.cs
@@ -2,12 +2,177 @@
 
 internal class Program
 {
-    static void Main(string[] args) { }
+    // Struct for the directions of adjacent cells
+    public struct CardinalDirection
+    {
+        public string Direction { get; set; }
+        public int Row { get; set; }
+        public int Col { get; set; }
+    }
+
+    static void Main(string[] args)
+    {
+        string inputPath = "input.txt";
+        //string inputPath = "testInput.txt";
+        string[] inputData = File.ReadAllLines(inputPath);
+        int numRows = inputData.Length;
+        int numCols = inputData[0].Length;
+        char[,] grid = InitializeGrid(inputData, numRows, numCols);
+
+        // Define the directions for adjacent cells
+        CardinalDirection[] directions =
+        [
+            new CardinalDirection
+            {
+                Direction = "North",
+                Row = -1,
+                Col = 0
+            },
+            new CardinalDirection
+            {
+                Direction = "NorthEast",
+                Row = -1,
+                Col = 1
+            },
+            new CardinalDirection
+            {
+                Direction = "East",
+                Row = 0,
+                Col = 1
+            },
+            new CardinalDirection
+            {
+                Direction = "SouthEast",
+                Row = 1,
+                Col = 1
+            },
+            new CardinalDirection
+            {
+                Direction = "South",
+                Row = 1,
+                Col = 0
+            },
+            new CardinalDirection
+            {
+                Direction = "SouthWest",
+                Row = 1,
+                Col = -1
+            },
+            new CardinalDirection
+            {
+                Direction = "West",
+                Row = 0,
+                Col = -1
+            },
+            new CardinalDirection
+            {
+                Direction = "NorthWest",
+                Row = -1,
+                Col = -1
+            },
+        ];
+
+        List<int> adjacentNumbers = FindAdjacentNumbers(grid, directions);
+
+        Console.WriteLine(
+            $"The sum of the numbers that are adjacent to a symbol is: {adjacentNumbers.Sum()}"
+        );
+    }
+
+    // Initializes 2D array of characters to represent the inputdata as a grid
+    public static char[,] InitializeGrid(string[] data, int numRows, int numCols)
+    {
+        char[,] grid = new char[numRows, numCols];
+        for (int row = 0; row < numRows; row++)
+        {
+            for (int col = 0; col < numCols; col++)
+            {
+                grid[row, col] = data[row][col];
+            }
+        }
+        return grid;
+    }
+
+    // Returns a List of all the numbers that are adjacent to a symbol
+    public static List<int> FindAdjacentNumbers(char[,] grid, CardinalDirection[] directions)
+    {
+        int numRows = grid.GetLength(0);
+        int numCols = grid.GetLength(1);
+        List<int> adjacentNumbers = [];
+
+        // Loop through the grid and find a symbol
+        for (int row = 0; row < numRows; row++)
+        {
+            for (int col = 0; col < numCols; col++)
+            {
+                if (IsSymbol(grid[row, col]))
+                {
+                    HashSet<int> candidateNumbers = [];
+                    // Check all adjacent tiles in the grid
+                    foreach (CardinalDirection direction in directions)
+                    {
+                        int candidateRow = row + direction.Row;
+                        int candidateCol = col + direction.Col;
+
+                        // Check if the candidate tile is valid, i.e. in bounds and a digit
+                        // If valid, extract the number and store it in a HashSet to avoid duplicates
+                        if (IsValidCandidate(grid, candidateRow, candidateCol))
+                        {
+                            candidateNumbers.Add(ExtractNumber(grid, candidateRow, candidateCol));
+                        }
+                    }
+
+                    // Add the valid candidates to the list of adjacent numbers
+                    adjacentNumbers.AddRange(candidateNumbers);
+                }
+            }
+        }
+
+        return adjacentNumbers;
+    }
+
+    // Helper method to evaluate if a character is a symbol
+    public static bool IsSymbol(char c)
+    {
+        return c != '.' && !char.IsDigit(c);
+    }
+
+    // Helper method to evaluate if a candidate tile is valid, i.e. within bounds and a digit
+    public static bool IsValidCandidate(char[,] grid, int row, int col)
+    {
+        int numRows = grid.GetLength(0);
+        int numCols = grid.GetLength(1);
+        bool inBounds = row >= 0 && row < numRows && col >= 0 && col < numCols;
+        bool isDigit = char.IsDigit(grid[row, col]);
+        return inBounds && isDigit;
+    }
+
+    // Finds the starting and ending coordinates of a number and returns it as an integer
+    public static int ExtractNumber(char[,] grid, int row, int col)
+    {
+        int startCol = col;
+        int endCol = col;
+
+        // Move left through the grid to find the beginning of the number
+        while (startCol > 0 && char.IsDigit(grid[row, startCol - 1]))
+        {
+            startCol--;
+        }
+
+        // Move right through the grid to find the end of the number
+        while (endCol < grid.GetLength(1) - 1 && char.IsDigit(grid[row, endCol + 1]))
+        {
+            endCol++;
+        }
+
+        // Construct the resulting number as a string
+        int numberLength = endCol - startCol + 1;
+        string number = "";
+        for (int i = 0; i < numberLength; i++)
+        {
+            number += grid[row, startCol + i];
+        }
+
+        return int.Parse(number);
+    }
 }
-
-
-// TODO: parse input text to be able to iterate through each coordinate
-// TODO: find a number and identify how long it is, then check all adjacent tiles and see if any is a symbol. If yes, store the number. Might lead to unneccesary lookups?
-//       OR
-//       find a symbol and check the adjacent tiles to see if there is a digit, then identify the number. See if the number has been stored already, otherwise store it.
-// TODO: calculate the sum of the numbers that are adjacent to a symbol. 
